1bf0a41d7870731d5cd6366aaebd971e378860e3
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-03-29 13:54:14 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [HttpClient, RouterOutlet],
     templateUrl: './app.component.html'
 })
 export class AppComponent {
