3f5d3806c8c76f5d769785d37d9055ccff2444e7
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-03-29 16:19:45 +0300

    auth.service.ts

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'https://localhost:7206/api/Auth/Login';
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: { uname: string; password: string }): Observable<any> {
+    return this.http.post<any>(this.apiUrl, credentials);
+  }
+}
