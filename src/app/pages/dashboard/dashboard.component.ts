4670bbcbe36936e09cd996085d047dd82fd09f2a
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-04-27 22:01:17 +0300

    ...

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { BlankComponent } from 'src/app/layouts/blank/blank.component';
 import { HeaderComponent } from 'src/app/layouts/full/header/header.component';
+import { SidebarComponent } from 'src/app/layouts/full/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   imports:[
     BlankComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ]
 })
 export class DashboardComponent{
